3ac0d2f865241e24835f8696f4fe76d315d873fd
Saursinet <florian.saurs@epitech.eu>
2018-02-28 16:16:44 +0700

    [FIX] - Push all files for init of the repository

---
+++ DeliverApp/LoginViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  DeliverApp
+//
+//  Created by Florian Saurs on 28/02/2018.
+//  Copyright © 2018 Florian Saurs. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let textFieldUnderlineColor = UIColor.init(red:55/255.0, green:61/255.0, blue:66/255.0, alpha: 1.0).cgColor
+        Helper.setTextFieldInvisibleWithBottomLine(textField: usernameTextField, color: textFieldUnderlineColor)
+        Helper.setTextFieldInvisibleWithBottomLine(textField: passwordTextField, color: textFieldUnderlineColor)
+        Helper.setValidateDesignToButton(button: loginButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func signIn(_ sender: Any) {
+        if (usernameTextField.text?.count == 0 || passwordTextField.text?.count == 0) {
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: "Sign in failed", message: "Mail address or password is incorrect.", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Sign in", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            return ;
+        }
+        loadMainView()
+//        DataController.login(email: usernameTextField.text!, password: passwordTextField.text!) { (success, message) -> () in
+//            if success {
+//                self.loadMainView()
+//            } else {
+//                DispatchQueue.main.async {
+//                    let alert = UIAlertController(title: "Sign in failed", message: "Mail address or password is incorrect.", preferredStyle: UIAlertControllerStyle.alert)
+//                    alert.addAction(UIAlertAction(title: "Sign in", style: UIAlertActionStyle.default, handler: nil))
+//                    self.present(alert, animated: true, completion: nil)
+//                }
+//            }
+//        }
+    }
+    
+    func loadMainView() {
+        DispatchQueue.main.async {
+            let appDelegate = UIApplication.shared.delegate! as! AppDelegate
+            
+            let initialViewController = self.storyboard!.instantiateViewController(withIdentifier: "NavigationController")
+            appDelegate.window?.rootViewController = initialViewController
+            appDelegate.window?.makeKeyAndVisible()
+        }
+    }
+}
+
